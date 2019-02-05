104012fec83d4bd07b5defcdb5764b2170b718a2
pratikjain04 <pratikjain0411@gmail.com>
2019-02-05 19:31:40 +0530

    Initial Commit
    Signed-off-by: pratikjain04 <pratikjain0411@gmail.com>

---
+++ cardanimation/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Upcoming(),
+      debugShowCheckedModeBanner: false,
+    ));
+
+class Upcoming extends StatefulWidget {
+  @override
+  _UpcomingState createState() => _UpcomingState();
+}
+
+class _UpcomingState extends State<Upcoming> {
+  @override
+  Widget build(BuildContext context) {
+    final uniWidth = MediaQuery.of(context).size.width;
+    final uniHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding:
+                      EdgeInsets.only(top: uniHeight / 10, left: uniWidth / 10),
+                  child: Text(
+                    'upcoming',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontWeight: FontWeight.w500,
+                      fontSize: uniWidth / 22,
+                      fontFamily: 'Poppins'
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
